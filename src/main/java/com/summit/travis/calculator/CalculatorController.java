dc188cef41ca8bdf53615b0d235b882ff9086d61
sftth <sftth318@gmail.com>
2019-10-11 15:57:59 +0900

    문자열 상수를 변수 활용으로 변경

--- src/main/java/com/summit/travis/calculator/CalculatorController.java
+++ src/main/java/com/summit/travis/calculator/CalculatorController.java
@@ -3,16 +3,20 @@ package com.summit.travis.calculator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class CalculatorController {
     @GetMapping("/cal")
     public String init() {
-        return "Calculator is being constructed.";
+        String returnStr = "Calculator is being constructed.";
+        String curTime = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        return returnStr + ": " +curTime;
     }
 
     @GetMapping("/login")
     public String login() {
         return "Login page";
     }
-
 }
